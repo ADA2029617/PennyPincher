7eba811387c29f8ebcce460b7030c30e422871b1
ADA2029617 <ada2029617@maricopa.edugit config --global user.email ada2029617@maricopa.edugit config --global user.email ada2029617@maricopa.edu>
2019-10-07 11:18:41 -0700

    Added actions and outlets for labels, text fields and buttons Added constants and variables for input and calculated values Added functions for show and hide the calculated labels

--- PennyPincher/ViewController.swift
+++ PennyPincher/ViewController.swift
@@ -9,10 +9,71 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //----------Attributes--------//
+    let hoursWorked = 0
+    var stateTax = 0
+    let grossPay = 0
+    let netPay = 0
+    let moveToSavings = 0
+    
+    
+    //-----------Outlets----------//
+    @IBOutlet weak var HoursTextField: UITextField!
+    @IBOutlet weak var StateTaxTextField: UITextField!
+    @IBOutlet weak var CalculateButtonOutlet: UIButton!
+    @IBOutlet weak var GrossPayLabel: UILabel!
+    @IBOutlet weak var GrossLabel: UILabel!
+    @IBOutlet weak var NetPayLabel: UILabel!
+    @IBOutlet weak var NetLabel: UILabel!
+    @IBOutlet weak var MoveToSavingsLabel: UILabel!
+    @IBOutlet weak var SavingsLabel: UILabel!
+    
+    
+    
+    
+    //-----------Actions----------//
+    @IBAction func CalculateButtonAction(_ sender: UIButton) {
+        ShowCalculatedLabels()
+    }
+    
+    
+    
+    
+    //----------Functions---------//
+    func ShowCalculatedLabels() {
+        GrossPayLabel.isHidden = false
+        GrossLabel.isHidden = false
+        NetPayLabel.isHidden = false
+        NetLabel.isHidden = false
+        MoveToSavingsLabel.isHidden = false
+        SavingsLabel.isHidden = false
+    }
+    
+    func HideCalculatedLabels() {
+        GrossPayLabel.isHidden = true
+        GrossLabel.isHidden = true
+        NetPayLabel.isHidden = true
+        NetLabel.isHidden = true
+        MoveToSavingsLabel.isHidden = true
+        SavingsLabel.isHidden = true
+    }
+    
+    func CalculateGrossPay() {
+        
+    }
+    
+    func CalculateNetPay() {
+        
+    }
+    
+    func CalculateSavingsTransfer() {
+        
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        HideCalculatedLabels()
     }
 
 

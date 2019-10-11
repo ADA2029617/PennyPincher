0139c144328607ee21817e1e4686cc6985548b7b
ADA2029617 <ada2029617@maricopa.edugit config --global user.email ada2029617@maricopa.edugit config --global user.email ada2029617@maricopa.edu>
2019-10-10 21:42:40 -0700

    Added error label and decision statement Added cash register sound when calculate button is pressed Added cash register sound to app files

--- PennyPincher/ViewController.swift
+++ PennyPincher/ViewController.swift
@@ -5,24 +5,35 @@
 //  Created by Adan Partida on 10/7/19.
 //  Copyright © 2019 Adan Partida. All rights reserved.
 //
+// TODO:
+// Handle nil from textfields
+// Add label and textfield for shift differential hours
+// Store previous net pay value as token for later use
+// Add decision statements to compare current and previous
+// net pays and change UIView background accordingly
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
     //----------Attributes--------//
     let hourlyWage = 11.25
+    let shiftDiffWage = 11.50
+    var shiftDiffTotal = 0.0
     var hoursWorked = 0.0
     var stateTax = 0.0
     var amountTaxed = 0.0
     var grossPay = 0.0
     var netPay = 0.0
     var moveToSavings = 0.0
+    var audioPlayer: AVAudioPlayer!
     
     
     //-----------Outlets----------//
     @IBOutlet weak var HoursTextField: UITextField!
     @IBOutlet weak var StateTaxTextField: UITextField!
+    @IBOutlet weak var ShiftDiffHoursTextField: UITextField!
     @IBOutlet weak var CalculateButtonOutlet: UIButton!
     @IBOutlet weak var GrossPayLabel: UILabel!
     @IBOutlet weak var GrossLabel: UILabel!
@@ -30,12 +41,21 @@ class ViewController: UIViewController {
     @IBOutlet weak var NetLabel: UILabel!
     @IBOutlet weak var MoveToSavingsLabel: UILabel!
     @IBOutlet weak var SavingsLabel: UILabel!
+    @IBOutlet weak var ErrorLabel: UILabel!
     
     
     
     
     //-----------Actions----------//
     @IBAction func CalculateButtonAction(_ sender: UIButton) {
+        
+        audioPlayer.play()
+        
+        if HoursTextField.text?.isEmpty ?? true || StateTaxTextField.text?.isEmpty ?? true || ShiftDiffHoursTextField.text?.isEmpty ?? true {
+            ErrorLabel.text = "Please fill all text fields."
+            ErrorLabel.isHidden = false
+        }
+        
         GrossLabel.text = "$" + String(format: "%.2f", CalculateGrossPay())
         NetLabel.text = "$" + String(format: "%.2f", CalculateNetPay())
         SavingsLabel.text = "$" + String(format: "%.2f", CalculateSavingsTransfer())
@@ -56,6 +76,7 @@ class ViewController: UIViewController {
     
     func HideCalculatedLabels() {
         GrossPayLabel.isHidden = true
+        ErrorLabel.isHidden = true
         GrossLabel.isHidden = true
         NetPayLabel.isHidden = true
         NetLabel.isHidden = true
@@ -64,9 +85,13 @@ class ViewController: UIViewController {
     }
     
     func CalculateGrossPay(/*_ workHours: Double */) -> Double {
+        
+        
         hoursWorked = Double(HoursTextField.text!)!
-        stateTax = Double(StateTaxTextField.text!)!
-        grossPay = hourlyWage * hoursWorked
+        stateTax = (Double(StateTaxTextField.text!)!) / 100
+        shiftDiffTotal = shiftDiffWage * Double(ShiftDiffHoursTextField.text!)!
+        grossPay = (hourlyWage * hoursWorked)
+        grossPay += shiftDiffTotal
         return grossPay
     }
     
@@ -80,10 +105,24 @@ class ViewController: UIViewController {
         moveToSavings = netPay * 0.20
         return moveToSavings
     }
+    
+    @IBAction func doneTyping(_ sender: UITextField) {
+        sender.resignFirstResponder()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         HideCalculatedLabels()
+        
+        let Sound = URL(fileURLWithPath: Bundle.main.path(forResource: "Cash Register", ofType: "mp3")!)
+        //let sound = Bundle.main.path(forResource: "Cash Register", ofType: "mp3")
+        
+        do {
+            audioPlayer = try AVAudioPlayer(contentsOf: Sound)
+        }
+        catch {
+            print(error)
+        }
     }
 
 
